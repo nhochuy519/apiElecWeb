9359ad475fd7ad1fb4d7baeca51910b6794d0c7e
nhochuy519 <huymini2009@gmail.com>
2024-08-05 14:21:28 +0700

    update cors

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser("ronaldoIsTheGoat"));
 app.use(function (req, res, next) {
