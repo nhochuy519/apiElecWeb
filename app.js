7440127401bbb64f0db9f47c380284c2860fc42f
nhochuy519 <huymini2009@gmail.com>
2024-08-05 16:26:57 +0700

    update cors

--- app.js
+++ app.js
@@ -16,8 +16,13 @@ const globalHanleError = require("./controller/errorController");
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
-app.use(cors());
 app.use(express.json());
 app.use(cookieParser("ronaldoIsTheGoat"));
 
